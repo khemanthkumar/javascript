dab0a79d4006c076b0a4c1004fa2ac6503b759f8
hemanth <hemanthkumar20121@gmail.com>
2021-12-21 17:04:05 +0530

    changes made

--- Greeting.js
+++ Greeting.js
@@ -4,9 +4,9 @@ import "./Greeting.css"
 function Greeting() {
     return (
         <div className='header'>
-            <h1>hemanth kumar.k</h1>
+            <h1>hemanth kumar</h1>
             <h1>i am new to devops</h1>
-            <h1>i am started to learn</h1>
+            
         </div>
     )
 }

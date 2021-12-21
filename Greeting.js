4ef2eeff2aafcbd5198ee3758590f87d6643f172
hemanth <hemanthkumar20121@gmail.com>
2021-12-21 16:56:56 +0530

    files added

---
+++ Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Greeting.css"
+
+function Greeting() {
+    return (
+        <div className='header'>
+            <h1>hemanth kumar.k</h1>
+            <h1>i am new to devops</h1>
+            <h1>i am started to learn</h1>
+        </div>
+    )
+}
+
+export default Greeting
